3630c57d71b05fe145b93d4460437fb84c00b691
unknown <you@example.com>
2019-01-22 10:28:08 +0100

    e

--- src/main/java/tpVolSpring/entity/Vol.java
+++ src/main/java/tpVolSpring/entity/Vol.java
@@ -15,6 +15,8 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
+import tpVolSpring.repository.CompagnieAerienneVol;
+
 
 @Entity
 @Table(name = "vol")
