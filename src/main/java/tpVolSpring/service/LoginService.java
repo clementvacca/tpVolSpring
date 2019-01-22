58f9a7b3c4fcfa8869b7ddbc581fb53e47f086a0
vacca <clement.vacca1@outlook.com>
2019-01-22 11:35:55 +0100

    test

--- src/main/java/tpVolSpring/service/LoginService.java
+++ src/main/java/tpVolSpring/service/LoginService.java
@@ -1,9 +1,11 @@
 package tpVolSpring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tpVolSpring.repository.LoginRepository;
 
+@Service
 public class LoginService {
 
 	@Autowired
--- src/main/java/tpVolSpring/service/PassagerService.java
+++ src/main/java/tpVolSpring/service/PassagerService.java
@@ -1,9 +1,11 @@
 package tpVolSpring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tpVolSpring.repository.PassagerRepository;
 
+@Service
 public class PassagerService {
 	
 	@Autowired
