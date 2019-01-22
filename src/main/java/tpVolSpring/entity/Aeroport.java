9bf4fdc322d1b525ea4a62079dd4384aec2fac60
unknown <you@example.com>
2019-01-22 10:16:06 +0100

    modif

--- src/main/java/tpVolSpring/entity/Aeroport.java
+++ src/main/java/tpVolSpring/entity/Aeroport.java
@@ -13,7 +13,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "airport")
 @SequenceGenerator(name = "seqAeroport", sequenceName = "seq_airport", allocationSize = 1, initialValue = 1)
@@ -27,58 +26,67 @@ public class Aeroport {
 	@ManyToOne
 	@JoinColumn(name = "id_city")
 	private Ville ville;
-	@OneToMany(mappedBy="aeroportDepart")
+	@OneToMany(mappedBy = "aeroportDepart")
 	private List<Vol> volsDepart;
-	@OneToMany(mappedBy="aeroportArrivee")
+	@OneToMany(mappedBy = "aeroportArrivee")
 	private List<Vol> volsArrivee;
-	@OneToMany(mappedBy="key.aeroport")
+	@OneToMany(mappedBy = "key.aeroport")
 	private List<Escale> escales;
-	
-	
-	public Aeroport () {
-		
+
+	public Aeroport() {
+
 	}
+
 	public Aeroport(String nomAeroport, Ville ville) {
 		super();
 		this.nomAeroport = nomAeroport;
 		this.ville = ville;
 	}
+
 	public Long getIdAeroport() {
 		return idAeroport;
 	}
-	
+
 	public String getNomAeroport() {
 		return nomAeroport;
 	}
+
 	public void setNomAeroport(String nomAeroport) {
 		this.nomAeroport = nomAeroport;
 	}
+
 	public Ville getVille() {
 		return ville;
 	}
+
 	public void setVille(Ville ville) {
 		this.ville = ville;
 	}
-	
-	
+
 	public List<Vol> getVolsDepart() {
 		return volsDepart;
 	}
+
 	public void setVolsDepart(List<Vol> volsDepart) {
 		this.volsDepart = volsDepart;
 	}
+
 	public List<Vol> getVolsArrivee() {
 		return volsArrivee;
 	}
+
 	public void setVolsArrivee(List<Vol> volsArrivee) {
 		this.volsArrivee = volsArrivee;
 	}
+
 	public List<Escale> getEscales() {
 		return escales;
 	}
+
 	public void setEscales(List<Escale> escales) {
 		this.escales = escales;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -86,6 +94,7 @@ public class Aeroport {
 		result = prime * result + ((idAeroport == null) ? 0 : idAeroport.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -102,11 +111,5 @@ public class Aeroport {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-}
 
 }
---
+++ src/main/java/tpVolSpring/repository/CompagnieAerienneVol.java
@@ -0,0 +1,82 @@
+package tpVolSpring.repository;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name = "compagnie_aerienne_vol")
+public class CompagnieAerienneVol {
+
+	@EmbeddedId
+	private CompagnieAerienneVolPk key;
+
+	@Column(name = "numero_vol_pk", length = 50)
+	private String numero;
+
+	@Version
+	private int version;
+
+	public CompagnieAerienneVol() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CompagnieAerienneVol(CompagnieAerienneVolPk key, String numero, int version) {
+		super();
+		this.key = key;
+		this.numero = numero;
+		this.version = version;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public CompagnieAerienneVolPk getKey() {
+		return key;
+	}
+
+	public void setKey(CompagnieAerienneVolPk key) {
+		this.key = key;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompagnieAerienneVol other = (CompagnieAerienneVol) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		return true;
+	}
+
+}
