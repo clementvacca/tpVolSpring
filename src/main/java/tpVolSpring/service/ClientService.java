d7b847329bbe4c3027ee368923c380f944d8449d
Th0md <thomas.druot@gmail.com>
2019-01-22 17:45:53 +0100

    Tests Client

--- src/main/java/tpVolSpring/service/ClientService.java
+++ src/main/java/tpVolSpring/service/ClientService.java
@@ -1,19 +1,42 @@
 package tpVolSpring.service;
 
+import java.util.Optional;
+import java.util.function.LongToIntFunction;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import tpVolSpring.entity.Client;
+import tpVolSpring.entity.ClientPhysique;
+import tpVolSpring.entity.Login;
 import tpVolSpring.repository.ClientEiRepository;
 import tpVolSpring.repository.ClientMoralRepository;
 import tpVolSpring.repository.ClientPhysiqueRepository;
+import tpVolSpring.repository.ClientRepository;
+import tpVolSpring.repository.LoginRepository;
 
 public class ClientService {
 	
+	@Autowired
+	private ClientRepository clientRepository;
 	@Autowired
 	private ClientPhysiqueRepository clientPhysiqueRepository;
 	@Autowired
 	private ClientMoralRepository clientMoralRepository;
 	@Autowired
 	private ClientEiRepository clientEiRepository;
+	@Autowired
+	private LoginRepository loginRepository;
+	
+	public void deleteClientWithLogin(Long id) {
+		Optional<Login> opt = loginRepository.findById(id);
+		if (opt.isPresent()) {
+			Login log = opt.get();
+			log.getClient().setLogin(null);
+			clientRepository.save(log.getClient());
+			clientRepository.deleteById(id);
+		}
+		
+	}
 	
 	
 	
