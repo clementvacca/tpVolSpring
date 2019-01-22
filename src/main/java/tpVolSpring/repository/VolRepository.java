50f10c62a110e1492ffb3243ccb47fc6a72c8089
Your Name <you@example.com>
2019-01-22 10:25:30 +0100

    modif

---
+++ src/main/java/tpVolSpring/repository/VolRepository.java
@@ -0,0 +1,9 @@
+package tpVolSpring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tpVolSpring.entity.Vol;
+
+public interface VolRepository extends JpaRepository<Vol, Long>{
+
+}
