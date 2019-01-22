148247d209aca9c156d1c2ddba242549afb26c12
Your Name <you@example.com>
2019-01-22 10:21:16 +0100

    modif

---
+++ src/main/java/tpVolSpring/repository/AeroportRepository.java
@@ -0,0 +1,9 @@
+package tpVolSpring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tpVolSpring.entity.Aeroport;
+
+public interface AeroportRepository extends JpaRepository<Aeroport, Long> {
+
+}
---
+++ src/main/java/tpVolSpring/repository/VilleRepository.java
@@ -0,0 +1,9 @@
+package tpVolSpring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tpVolSpring.entity.Ville;
+
+public interface VilleRepository extends JpaRepository<Ville, Long> {
+
+}
